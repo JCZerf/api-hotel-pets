88a63d0292b70f27aef673972077c4e1abfec448
José Carlos <josecarlosmrlt@Outlook.com>
2025-05-29 18:26:11 -0300

    Implementando: Get e Post na API

--- index.js
+++ index.js
@@ -11,3 +11,20 @@ app.get('/', (req, res) => { //Definindo a rota para o endereço da raiz da API
 app.listen(port, () => {
     console.log(`Servidor online http:/localhost:${port}`); //Fazendo o servidor escutar a porta escolhida
 })
+
+let nomeTutor = [
+    { id: 1, nome: 'José Calos'}
+];
+
+app.get('/hotelPets', (req, res) => {
+    res.json(nomeTutor);
+});
+
+app.post('/hotelPets', (req, res) => {
+    const novoTutor = {
+        id: nomeTutor.length + 1,
+        nome: req.body.nome
+    }
+      nomeTutor.push(novoTutor);
+      res.status(201).json(novoTutor);
+});
